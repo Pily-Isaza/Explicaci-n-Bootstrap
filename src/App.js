75dc2c1fe407aca19acb8a82875bcb5863f3edba
Pilar <pily.isaza@gmail.com>
2021-09-28 07:37:22 -0500

    explicacion bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div classname='App'>
+    <div className='App'>
       <span>Hola Mundo</span>
-    </div>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" className="btn btn-danger">
+        Danger
+      </button>
+      <div class="alert alert-primary" role="alert">
+         A simple primary alert—check it out!
+      </div>
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..."/>
+      </div>
+      </div>
   );
 }
 
